42a30c074e1f4c24daa69ff6262084b244ef0990
Girish <nucchagirish0710@gmail.com>
2023-02-08 13:04:24 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ docker rm -f html_container || true;
 docker image rm -f html2_girish || true;
 docker build -t html2_girish .;
 docker run -dit --name html_container -p 80:80 html2_girish;
-docker login -u Giri87 -p Girish-AWS22;
-docker tag html2_girish Giri87/html2_girish;
-docker push Giri87/html2_girish
+docker login -u giri87 -p Girish-AWS22;
+docker tag html2_girish giri87/html2_girish;
+docker push giri87/html2_girish
