505c49f0e21ab124ab0b3defcb58d7ff7214abbe
Girish <nucchagirish0710@gmail.com>
2023-02-08 12:35:25 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 pwd;
 docker stop html_container || true;
-docker rm -f html-container || true;
+docker rm -f html_container || true;
 docker image rm -f html2_girish || true;
 docker build -t html2_girish .;
 docker run -dit --name html_container -p 80:80 html2_girish;
